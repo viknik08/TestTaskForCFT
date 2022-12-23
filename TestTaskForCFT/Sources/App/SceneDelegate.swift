964f773888c050f5f8de5cd32fd05384a5a85f21
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-12-23 14:10:05 +0800

    preparing project

---
+++ TestTaskForCFT/Sources/App/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  TestTaskForCFT
+//
+//  Created by Виктор Басиев on 23.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
